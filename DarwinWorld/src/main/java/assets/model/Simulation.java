9d2e09275ae0c32747569559c32bb2de241ca005
Kacper Zajac <kacper.zajac004@gmail.com>
2024-12-22 16:53:35 +0100

    simulation class init

---
+++ DarwinWorld/src/main/java/assets/model/Simulation.java
@@ -0,0 +1,14 @@
+package assets.model;
+
+public class Simulation implements Runnable{
+    private WorldMap map;
+    
+    public Simulation() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}

043481dbaed0e28ae21ace6b4b87ae6a71afd7fb
Xenoneqq <realfatboimlg@gmail.com>
2025-01-11 15:02:15 +0100

    Auto stash before checking out "origin/main"

--- DarwinWorld/src/main/java/assets/model/application/SimulationApp.java
+++ DarwinWorld/src/main/java/assets/model/application/SimulationApp.java
@@ -4,14 +4,19 @@ import assets.SimulationManager;
 import assets.model.contract.MapChangeListener;
 import assets.model.map.AbstractMap;
 import assets.model.util.ConsoleMapPrinter;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SimulationApp implements MapChangeListener {
+
+    @FXML
+    private GridPane mapGrid;
 
     public void launchSimulation() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("simulation-window.fxml"));
@@ -24,7 +29,7 @@ public class SimulationApp implements MapChangeListener {
         stage.show();
     }
 
-    public void drawMap(){
+    public void drawMap(BaseMap map){
 
     }
 
